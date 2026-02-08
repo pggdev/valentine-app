fdcaaf65ed2dab92061f848affb452e23527bf0a
princegupta1234 <princegupta.ns153@gmail.com>
2026-02-08 21:59:25 +0530

    add vercel website analytics tool

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google"; // Importing a cute rounded font
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${nunito.className} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
